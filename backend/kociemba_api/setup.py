5aed9b7dade9430650b45e0457006af056141676
abhinavdogra21 <dograabhinav21@gmail.com>
2025-07-08 16:39:45 +0530

    fix: use pybind11.get_include() in setup.py

--- backend/kociemba_api/setup.py
+++ backend/kociemba_api/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, Extension
 import os
+import pybind11
 
 ext_modules = [
     Extension(
@@ -7,7 +8,7 @@ ext_modules = [
         ["src/kociemba_wrapper.cpp", 
          "src/solver/cube.cpp",
          "src/solver/solver.cpp"],
-        include_dirs=["src", "pybind11/include"],
+        include_dirs=[pybind11.get_include(), "src"],
         language='c++',
         extra_compile_args=['-std=c++11'],
     ),
@@ -18,4 +19,4 @@ setup(
     ext_modules=ext_modules,
     install_requires=["pybind11>=2.6.0"],
     python_requires=">=3.6",
-) 
+)
